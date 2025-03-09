9f466cd13e6a8575f604f086b57d58afdc56a4cc
Norman Douglas <smokewulf@yahoo.com>
2025-03-08 19:43:40 -0600

    commit

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ from gptauthor.console import create_new_story
 
 app = FastAPI()
 
+#Define the request body schema                                                                                                                                  
+class StoryRequest(BaseModel):                                                                                                                                    
+    title: str                                                                                                                                                    
+    genre: str                                                                                                                                                    
+    description: str
+
 @app.get("/clone")
 def clone_repo():
     os.system("git clone https://github.com/smokewulf/gptauthor.git")
@@ -14,3 +20,9 @@ def clone_repo():
 def list_commits():
     commits = os.popen("git --git-dir=gptauthor/.git log --oneline").read()
     return {"commits": commits.splitlines()}
+
+@app.post("/create_story")                                                                                                                                        
+def create_story(story_request: StoryRequest):                                                                                                                    
+    # Call the function from console.py to create a new story                                                                                                     
+    result = create_new_story(story_request.title, story_request.genre, story_request.description)                                                                
+    return {"message": result}
